45555fa89d9b338bd750223b484a829d6b061ac6
Sarvar Jafarov <sarvar.jafarov@gmail.com>
2025-11-11 21:56:05 -0500

    new error

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Models\Job;
 use App\Observers\JobObserver;
+use Illuminate\Contracts\View\Factory as ViewFactory;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,14 +14,25 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $viewPath = resource_path('views');
+        $configuredPaths = config('view.paths', []);
+
+        if (! in_array($viewPath, $configuredPaths, true)) {
+            config(['view.paths' => array_merge([$viewPath], $configuredPaths)]);
+        }
     }
 
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+    public function boot(ViewFactory $view): void
     {
         Job::observe(JobObserver::class);
+
+        $viewPath = resource_path('views');
+
+        if (! in_array($viewPath, $view->getFinder()->getPaths(), true)) {
+            $view->addLocation($viewPath);
+        }
     }
 }
